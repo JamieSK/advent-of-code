800350feedc2aaf6ada40153640fe6f5cc085831
Jamie Klein <jamie.klein@multiverse.io>
2022-12-06 14:11:41 +0000

    day 1

--- lib/advent_of_code/day01.ex
+++ lib/advent_of_code/day01.ex
@@ -7,7 +7,7 @@ defmodule AdventOfCode.Day01 do
 
   @impl true
   def run do
-    {:ok, input} = File.read("resources/day01/input.txt")
+    input = File.stream!("resources/day01/input.txt")
 
     IO.puts("--- Part One ---")
     IO.puts("Result: #{part_one(input)}")
@@ -16,9 +16,36 @@ defmodule AdventOfCode.Day01 do
     IO.puts("Result: #{part_two(input)}")
   end
 
-  def part_one(_args) do
+  def part_one(args) do
+      args
+      |> Enum.reduce({0, 0}, fn line, {current_max, running_total} ->
+      line
+      |> String.trim()
+      |> case do
+        "" -> {max(current_max, running_total), 0}
+        str -> {current_max, running_total + String.to_integer(str)}
+      end
+    end)
+    |> elem(0)
   end
 
-  def part_two(_args) do
+  def part_two(args) do
+    args
+    |> Enum.reduce({[0, 0, 0], 0}, fn line, {current_maxes, running_total} ->
+      line
+      |> String.trim()
+      |> case do
+        "" ->
+          {
+            [running_total | current_maxes]
+            |> Enum.sort(&(&1 >= &2))
+            |> Enum.drop(-1),
+            0
+          }
+        str -> {current_maxes, running_total + String.to_integer(str)}
+      end
+    end)
+    |> elem(0)
+    |> Enum.sum()
   end
 end
